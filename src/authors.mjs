257097792931fa8491b4692e79a24120a420d62a
Darren Griffin <darren.griffin@live.co.uk>
2026-03-25 13:18:17 +0000

    Add 2.8 release blog and Claude skill (#556)
    * Add blog post and Claude skill for future posts
    
    * Fix image radius
    
    * Fix radius for blog list too

---
+++ src/authors.mjs
@@ -0,0 +1,14 @@
+export const authors = {
+  marcel: {
+    name: "Marcel van der Veldt",
+    title: "Ecosystems Lead",
+    picture: "https://avatars.githubusercontent.com/marcelveldt?size=64",
+    url: "https://github.com/marcelveldt",
+  },
+  marvin: {
+    name: "Marvin Schenkel",
+    title: "Tech Lead",
+    picture: "https://avatars.githubusercontent.com/marvinschenkel?size=64",
+    url: "https://github.com/marvinschenkel",
+  },
+};
