64f74fd128f4853a4c4c62b65bec1fc40d670421
Darren <darren.griffin@live.co.uk>
2026-08-13 14:01:31 +0000

    Revert "Add blog post"
    This reverts commit 57d97ad1189d0d779be4cdf7163f10ce6ac05306.

--- src/authors.mjs
+++ src/authors.mjs
@@ -17,10 +17,4 @@ export const authors = {
     picture: "https://avatars.githubusercontent.com/mrdarrengriffin?size=64",
     url: "https://github.com/mrdarrengriffin",
   },
-  missy: {
-    name: "Missy Quarry",
-    title: "Community Manager",
-    picture: "https://avatars.githubusercontent.com/missyquarry?size=64",
-    url: "https://github.com/missyquarry",
-  },
 };
